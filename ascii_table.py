c2048573e482ef14e063b7dfab47b64cfa06e9a2
Red <gcaaa31928@gmail.com>
2015-11-01 12:44:42 +0800

    * support vertical text

--- ascii_table.py
+++ ascii_table.py
@@ -1,5 +1,6 @@
 import re
 import sys
+import itertools
 
 __author__ = 'GCA'
 
@@ -15,6 +16,7 @@ def give_value_to_2d_map(dic, x, y, val):
         dic[x] = {}
     dic[x][y] = val
 
+
 # for constant variable
 
 first_row_regexp = '^┌.*┐$'
@@ -25,7 +27,7 @@ separated_content_symbol = '│'
 separated_line_started_symbol = '├'
 separated_line_end_symbol = '┤'
 separated_line_intersect_symbol = '┼'
-
+vertical_sensitive_header_keywords = ["事實"]
 
 file_name = input()
 
@@ -38,6 +40,7 @@ line_break_table = {}
 max_column = 0
 
 # get the column count by table header
+
 for ch in contentList[0]:
     if ch == first_row_intersect_symbol or ch == first_row_last_symbol:
         max_column += 1
@@ -92,7 +95,6 @@ for line_index, line in enumerate(contentList):
 
     separated_table[line_index] = separated
     line_break_table[line_index] = line_break
-    # print(separated)
 
 # compress the table
 '''
@@ -120,7 +122,7 @@ for line_break_index, line_break_key in enumerate(line_break_keys):
     normal_compress_map[line_break_key] = len(compress_break_table) - 1
     for line_break_cell in line_break:
         if line_break_cell == -1:
-            compress_is_line.append(len(compress_break_table) -1 )
+            compress_is_line.append(len(compress_break_table) - 1)
             break
 
 
@@ -149,7 +151,9 @@ for row_index, line_break in enumerate(compress_break_table):
 
 
 # concat cell content if they are the same cell.
+compress_content_list_table = {}
 compress_content_table = {}
+compress_width_count_table = {}
 for line_index in separated_table:
     separated = separated_table[line_index]
     line_break = line_break_table[line_index]
@@ -159,18 +163,40 @@ for line_index in separated_table:
     compress_line_index = normal_compress_map[line_index]
     if compress_line_index not in compress_content_table:
         compress_content_table[compress_line_index] = {}
-    previous_index = 0
+    if compress_line_index not in compress_width_count_table:
+        compress_width_count_table[compress_line_index] = {}
+    if compress_line_index not in compress_content_list_table:
+        compress_content_list_table[compress_line_index] = {}
+    previous_pos = 0
     for index, pos in enumerate(separated):
         if line_break[index] != -1:
             if index not in compress_content_table[compress_line_index]:
                 compress_content_table[compress_line_index][index] = ""
-            compress_content_table[compress_line_index][index] += line_content[previous_index + 1:pos]
-        previous_index = pos
+            if index not in compress_width_count_table[compress_line_index]:
+                compress_width_count_table[compress_line_index][index] = 0
+            if index not in compress_content_list_table[compress_line_index]:
+                compress_content_list_table[compress_line_index][index] = []
+            compress_content_table[compress_line_index][index] += line_content[previous_pos + 1:pos]
+            compress_content_list_table[compress_line_index][index].append(line_content[previous_pos + 1:pos])
+            compress_width_count_table[compress_line_index][index] = max(
+                compress_width_count_table[compress_line_index][index], pos - previous_pos - 1)
+
+        previous_pos = pos
+
+
+# flag the keyword column for vertical text
+vertical_column = []
+for column_key in compress_content_table[0]:
+    for keyword in vertical_sensitive_header_keywords:
+        if keyword in compress_content_table[0][column_key]:
+            vertical_column.append(column_key)
 
 # combined all the cell content if then are merged cell.
+
 compress_combined_table = {}
 compress_content_table_keys = list(compress_content_table.keys())
 for line_index, line in enumerate(compress_content_table):
+
     for column_index, column in enumerate(compress_content_table[line]):
 
         if line in span_count_table and column in span_count_table[line]:
@@ -181,11 +207,26 @@ for line_index, line in enumerate(compress_content_table):
         while i < line_index + remain_line_count:
             iterator_line_key = compress_content_table_keys[i]
             compress_content_table[line][column] += compress_content_table[iterator_line_key][column]
+            compress_content_list_table[line][column] += compress_content_list_table[iterator_line_key][column]
             compress_content_table[iterator_line_key][column] = ""
+            compress_content_list_table[iterator_line_key][column] = []
             give_value_to_2d_map(compress_combined_table, iterator_line_key, column, 1)
             if iterator_line_key in compress_is_line:
                 remain_line_count += 1
             i += 1
+
+# turn into vertical text
+for line in compress_content_table:
+    for column in vertical_column:
+        if line not in compress_content_list_table or column not in compress_content_list_table[line]:
+            continue
+
+        origin_text = compress_content_list_table[line][column]
+        vertical_list = list(map(list, (itertools.zip_longest(*origin_text))))
+        vertical_list_content = [''.join(char for char in context if char) for context in vertical_list]
+        vertical_list_content = ''.join(vertical_list_content)
+        vertical_text = vertical_list_content
+        compress_content_table[line][column] = vertical_text
 
 header = """
 <!DOCTYPE html>
@@ -230,10 +271,7 @@ for line in compress_content_table:
             tr_content += ">" + compress_content_table[line][column] + "</td>\n"
     if tr_content != "":
         html_content += "<tr>\n" + tr_content + "</tr>\n"
-# print(html_content)
-
 
 f = open('test_ascii_table.html', 'w', encoding='utf-8')
 f.write(header + html_content + footer)
 f.close()
-
